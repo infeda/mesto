01a3b8f9af29ff7042358f328c00cdfc5232af4a
Ingrida <ingrida.surname@gmail.com>
2021-01-07 10:05:50 +0300

    fixed: webpack config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import './pages/index.css';
-import Card from './script/components/Card.js';
-import FormValidator from './script/components/FormValidator.js';
-import { initialCards, config } from './script/utils.js'; 
-import Section from './script/components/Section.js';
-import PopupWithImage from './script/components/PopupWithImage.js';
-import PopupWithForm from './script/components/PopupWithForm.js';
-import UserInfo from './script/components/UserInfo.js';
+import './index.css';
+import Card from '../script/components/Card.js';
+import FormValidator from '../script/components/FormValidator.js';
+import { initialCards, config } from '../script/utils.js'; 
+import Section from '../script/components/Section.js';
+import PopupWithImage from '../script/components/PopupWithImage.js';
+import PopupWithForm from '../script/components/PopupWithForm.js';
+import UserInfo from '../script/components/UserInfo.js';
 
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
