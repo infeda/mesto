f57eb321bc152c1220a2ced30daa5d913483995f
Ingrida <ingrida.surname@gmail.com>
2021-01-07 11:56:08 +0300

    probnij

--- src/script/components/FormValidator.js
+++ src/script/components/FormValidator.js
@@ -21,6 +21,7 @@ export default class FormValidator {
       });
     });
 
+    
     this._formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
       this.resetValidation();
